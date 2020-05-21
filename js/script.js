61684e4180a445e10b13e1066a381c53c3c0e6a9
Rodrigo Silva <rdrsantos018@gmail.com>
2020-05-21 14:01:50 -0300

    Otimização modais
    Otimização no codigo dos modais

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 //Modal projetos
 function modal (projetoId, btn, modalItem){
     let projeto = document.querySelector(`.${projetoId}`)
-    let btnClose = document.querySelector(`.${btn}`)
-    let modalTarget = document.querySelector(`.${modalItem}`)    
+    let btnClose = document.querySelector(`#${btn}`)
+    let modalTarget = document.querySelector(`#${modalItem}`)    
     projeto.addEventListener("click", () => {
         modalTarget.classList.add("modal-on")
     })
@@ -10,10 +10,10 @@ function modal (projetoId, btn, modalItem){
         modalTarget.classList.remove("modal-on")
     })
 }
-modal("pjt-1", "btn-close-1", "modal-container-1")
-modal("pjt-2", "btn-close-2", "modal-container-2")
-modal("pjt-3", "btn-close-3", "modal-container-3")
-modal("pjt-4", "btn-close-4", "modal-container-4")
-modal("pjt-5", "btn-close-5", "modal-container-5")
-modal("pjt-6", "btn-close-6", "modal-container-6")
+modal("pjt-1", "btn1", "modal1")
+modal("pjt-2", "btn2", "modal2")
+modal("pjt-3", "btn3", "modal3")
+modal("pjt-4", "btn4", "modal4")
+modal("pjt-5", "btn5", "modal5")
+modal("pjt-6", "btn6", "modal6")
 //Modal projetos
