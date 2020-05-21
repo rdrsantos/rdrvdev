ff5d9206b81e0f05532c17ba5bda05a01e085ba3
Rodrigo Silva <rdrsantos018@gmail.com>
2020-05-21 15:49:18 -0300

    adicionei animações
    quadados animados ficam aparecendo na tela

--- js/script.js
+++ js/script.js
@@ -17,3 +17,37 @@ modal("pjt-4", "btn4", "modal4")
 modal("pjt-5", "btn5", "modal5")
 modal("pjt-6", "btn6", "modal6")
 //Modal projetos
+
+//squares
+
+function squares(ulId){
+
+const ulSquares = document.querySelector(`#${ulId}`)
+for(let i = 0; i < 11; i++){
+    const li = document.createElement("li")
+    const random = (min, max) => Math.random() * (max - min) + min
+    const size = Math.floor(random(5, 100))
+    const position = random(1, 99)
+    const delay = random(5, 0.1)
+    const duration = random(1, 20)
+
+    li.style.width = `${size}px`
+    li.style.height = `${size}px`
+    li.style.bottom = `-${size}px`
+    li.style.left = `${position}%`
+    li.style.animationDelay = `${delay}s`
+    li.style.animationDuration = `${duration}s`
+    li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()},, ${Math.random()})`
+    if(ulId == "squaresB" || ulId == "squaresB1"){
+        li.style.backgroundColor = "rgba(0, 6, 102, .10)"
+    }
+    ulSquares.appendChild(li)
+}}
+squares("squaresInicio")
+squares("squaresTec")
+squares("squaresContato")
+//squares
+
+// Animações scroll
+
+// Animações scroll
